8e356973b5fa142ccb71841f30e5e01d29c1a528
Alex Weiss <alex.m.weiss2@gmail.com>
2019-02-16 17:16:51 -0500

    start of lessons page

---
+++ lib/api.dart
@@ -0,0 +1,25 @@
+import 'package:shared_preferences/shared_preferences.dart';
+import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
+import 'dart:convert';
+
+class API {
+  static Future<String> getTopicsToLearn(String email) {
+    debugPrint("started api call");
+    var url = "https://us-central1-nexgenmath.cloudfunctions.net/getTopicsToLearn?email=" + email;
+    debugPrint(url);
+    return http.post(url).then((response) {
+      debugPrint("finished api call");
+      return response.body;
+    });
+  }
+  static Future<String> getTopicsToPractice(String email) {
+    debugPrint("started api call");
+    var url = "https://us-central1-nexgenmath.cloudfunctions.net/getTopicsToPractice?email=" + email;
+    debugPrint(url);
+    return http.post(url).then((response) {
+      debugPrint("finished api call");
+      return response.body;
+    });
+  }
+}
